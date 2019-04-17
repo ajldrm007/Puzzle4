dcd93d5053f371a6b1667605027ec989710ef452
andy <ajldrm007@gmail.com>
2019-04-17 01:34:15 -0400

    adding  Puzzle4 base structure

--- src/com/company/Puzzle4.java
+++ src/com/company/Puzzle4.java
@@ -4,6 +4,36 @@ public class Puzzle4 {
 
     public static void main(String[] args) {
         // write your code here
+
+        Puzzle4b [] obs = new Puzzle4b[6];
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        while (x<6){
+            //
+            //something here
+            y = y * 10;
+            x = x - 1;
+        }
+        x = 6;
+        while (x>0){
+            //something here
+            result = result + //something here
+        }
+        System.out.println("result " + result);
+
     }
 
 }
+
+class Puzzle4b(){
+        int ivar;
+        private int doStuff(int factor)
+        {
+        if(ivar>100){
+        return //something here
+        }else{
+        return //something here
+        }
+        }
+        }
